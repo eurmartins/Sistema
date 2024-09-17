8f690df0384a5f688d6439e721dbe38c480fa340
eurmartins <vitaocap123@gmail.com>
2024-09-16 23:28:53 -0300

    primeiro commit do sistema

---
+++ src/main/java/com/projeto/sistema/repository/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.sistema.repository;
+
+
+import com.projeto.sistema.models.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
